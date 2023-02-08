f936d6922f5dbc163070d5325e4aa7a8d4b7950d
ExistedGit <66470490+ExistedGit@users.noreply.github.com>
2023-02-08 15:40:43 +0100

    + IsRoshan
    * Moved Debug functions to a new header

--- Dota2Cheat/UIState.h
+++ Dota2Cheat/UIState.h
@@ -6,6 +6,7 @@ namespace UIState {
 	namespace Menu {
 
 	}
+
 	inline const char* WeatherList[] = { "Default", "Winter", "Rain", "MoonBeam", "Pestilence", "Harvest", "Sirocco", "Spring", "Ash", "Aurora" };
 
 	inline bool HeroVisibleToEnemy = false;
