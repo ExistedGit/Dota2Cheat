bb93a87109c590a72de0d28eae747916bbe6c74e
ExistedGit <66470490+ExistedGit@users.noreply.github.com>
2023-02-14 18:06:29 +0100

    IEngineClient changed to use CallVFunc

--- Dota2Cheat/IEngineClient.h
+++ Dota2Cheat/IEngineClient.h
@@ -7,11 +7,11 @@ class IEngineClient :
 public:
 	inline int GetLocalPlayerSlot() {
 		int idx = 0;
-		GetVFunc(20)(this, &idx, 0, 0);
+		CallVFunc<20>(&idx, 0, 0);
 		return idx;
 	}
 	inline bool IsInGame() {
-		return (bool)this->GetVFunc(25).Execute<unsigned char>(this);
+		return (bool)CallVFunc<25, unsigned char>();
 	}
 
 };
