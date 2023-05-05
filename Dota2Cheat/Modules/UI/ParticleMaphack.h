2c5a81b3494b40cc6b177b375cdc7f67762bd665
ExistedGit <66470490+ExistedGit@users.noreply.github.com>
2023-05-05 17:56:27 +0200

    + Imporved Particle MapHack

--- Dota2Cheat/Modules/UI/ParticleMaphack.h
+++ Dota2Cheat/Modules/UI/ParticleMaphack.h
@@ -15,6 +15,9 @@ namespace Hacks {
 			CDOTABaseNPC* ent{};
 		};
 
+		// Particles which are created with attach type 2 and then are transformed with separate messages
+		std::map<int, CDOTABaseNPC_Hero*> TransformQueue;
+
 		// Only shows the last one
 		std::map<CDOTABaseNPC*, ParticleData> MapAppearances;
 		std::vector<ParticleEntData> AllAppearances;
@@ -22,10 +25,29 @@ namespace Hacks {
 		float lastTime = 0;
 		void DrawAllAppearances();
 		void DrawMapAppearances();
+		void RegisterAppearance(CDOTABaseNPC* npc, const Vector& pos) {
+			ImTextureID icon = 0;
+			GetHeroIcon(npc, icon);
+
+			auto& mapData = MapAppearances[npc];
+			mapData.pos = pos;
+			mapData.fadeCounter = mapData.fadeTime = Config::ParticleMapHack::FadeDuration;
+			mapData.icon = icon;
+
+			auto data = ParticleEntData{
+				.ent = npc
+			};
+
+			data.pos = pos;
+			data.icon = icon;
+			data.fadeCounter = data.fadeTime = Config::ParticleMapHack::FadeDuration;
+			AllAppearances.push_back(data);
+		}
 	public:
 		void Draw();
 		void FrameBasedLogic();
 		void ProcessParticleMsg(NetMessageHandle_t* msgHandle, google::protobuf::Message* msg);
+		void GetHeroIcon(CDOTABaseNPC* npc, ImTextureID& icon);
 	};
 }
 namespace Modules {
